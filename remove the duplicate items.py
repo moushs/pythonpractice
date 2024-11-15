4c79ecbba396c4c5db7ebdcc6f4d19f8902ed5fa
moushs <sarkarmousumi28926@gmail.com>
2024-11-15 15:12:20 +0530

    Create remove the duplicate items.py

---
+++ remove the duplicate items.py
@@ -0,0 +1,11 @@
+#write a program to remove the duplicate items from the list
+size_of_list=int(input())
+list_1=list(map(int,input().split()))
+b=[]
+unique=[]
+for x in list_1:
+    if x not in b:
+        unique.append(x)
+        b.append(x)
+print("Non duplicate items:")
+print(unique)
