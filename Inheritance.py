886167aaf6ce915f1b41a542210df95cb0ec96b3
moushs <sarkarmousumi28926@gmail.com>
2024-11-26 14:29:09 +0530

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -22,3 +22,33 @@ dog=Dog()
 cat=Cat()
 dog.sound()
 cat.sound()
+
+
+x=int(input("Enter the first number"))
+y=int(input("Enter the second number"))
+class calculate:
+    def sum(self,x,y):
+      self.x=x
+      self.y=y
+      print(self.x+self.y)
+
+    def sub(self,x,y):
+      self.x=x
+      self.y=y
+      print(self.x-self.y)
+
+class AdvanceCalculate(calculate):
+    def mul(self,x,y):
+      self.x=x
+      self.y=y
+      print(self.x*self.y)
+
+    def div(self,x,y):
+      self.x=x
+      self.y=y
+      print(self.x/self.y)
+c=AdvanceCalculate()
+c.sum(x,y)
+c.sub(x,y)
+c.mul(x,y)
+c.div(x,y)
