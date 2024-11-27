242eb432a49b40ce8496671bea46b970bc295cb6
moushs <sarkarmousumi28926@gmail.com>
2024-11-27 11:03:21 +0530

    Create input for 2 list.py

---
+++ input for 2 list.py
@@ -0,0 +1,5 @@
+#write a python program to get the input from the user for creating two lists and add the elements of both the list
+list1=list(input("enter the list1"))
+list2=list(input("enter the list2"))
+list3=list1+list2
+print(list3)
