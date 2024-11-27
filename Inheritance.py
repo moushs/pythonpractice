01313631fd9f3d77056f50334095438de754c4bc
moushs <sarkarmousumi28926@gmail.com>
2024-11-27 09:28:16 +0530

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -52,3 +52,25 @@ c.sum(x,y)
 c.sub(x,y)
 c.mul(x,y)
 c.div(x,y)
+
+
+class Calculator:
+  def add(self,num1,num2):
+    self.num1=num1;
+    self.num2=num2;
+    return self.num1+self.num2
+  def sub(self,num1,num2):
+    return num1-num2;
+class AdvancedCalculator(Calculator):
+  def mul(self,num1,num2):
+    print("Multiplication:",num1*num2)
+  def div(self,num1,num2):
+    return num1//num2;
+x=AdvancedCalculator()
+num1=int(input())
+num2=int(input())
+print("addition : %d"%(x.add(num1,num2)))
+print("subtract",x.sub(num1,num2))
+x.mul(num1,num2)
+print("floor division: %d"%(x.div(num1,num2)))    
+
