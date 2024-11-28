1b35d4511e908bdbb4e437eac251e1828e750491
moushs <sarkarmousumi28926@gmail.com>
2024-11-28 10:25:52 +0530

    Update Multiple_Inheritance.py

--- Multiple_Inheritance.py
+++ Multiple_Inheritance.py
@@ -46,3 +46,58 @@ suite_rent = derived_room.suite_calculates(3, 7)
 print(f"Normal Room Rent: {normal_rent}")
 print(f"AC Room Rent: {ac_rent}")
 print(f"Suite Room Rent: {suite_rent}")
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#write a python program to calculate the salary of the temporary staff using Multilevel Inheritance
+#Create a class person which contains a constructor init() and a method display(self).The method displays the name of the person.
+#Create another class Staff which inherit Person.It contains a constuctor init() and a method display(self).The method displays Id.
+#Create another class Temporarystaff which inherit Staff it also contains a constuctor init() and 2 method display(self), Salary(self).
+#The method Salary(self) return the total salary earned . The method display(self) displays number of days, hoursworked and total salary earned.
+#salary earned=total hours worked*150
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def display(self):
+        print("Name:", self.name)
+
+
+class Staff(Person):
+    def __init__(self, name, Id):
+        super().__init__(name)
+        self.id = Id
+
+    def display(self):
+        super().display()
+        print("ID:", self.id)
+
+
+class TemporaryStaff(Staff):
+    def __init__(self, name, Id, days, hoursworked):
+        super().__init__(name, Id)
+        self.days = days
+        self.hoursworked = hoursworked
+
+    def Salary(self):
+        salary = self.days * self.hoursworked * 150
+        return salary
+
+    def display(self):
+        super().display()
+        print("No of days:", self.days)
+        print("No of hours worked:", self.hoursworked)
+        print("Total Salary:", self.Salary())
+
+
+
+name = input("Enter name: ")
+Id = int(input("Enter ID: "))
+days = int(input("Enter number of days worked: "))
+hoursworked = int(input("Enter hours worked per day: "))
+
+temp_staff = TemporaryStaff(name, Id, days, hoursworked)
+
+
+temp_staff.display()
